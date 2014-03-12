9e0e9dbae15cfdcc308fa325c15c7100044ce243
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-03-12 14:00:34 +0000

    Detect architecture in Windows
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- fiji.c
+++ fiji.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+static int is_64bit(void)
+{
+	const char *architecture = getenv("PROCESSOR_ARCHITEW6432");
+
+	return architecture && !strcasecmp("amd64", architecture);
+}
 
 int main(int argc, const char *const *argv)
 {
-fprintf(stderr, "%d arguments\n", argc);
+fprintf(stderr, "%d arguments, 64-bit: %d\n", argc, is_64bit());
 	return execvp("echo.exe", argv);
 }
